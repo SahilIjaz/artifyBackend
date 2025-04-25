39d988716fd941c5478d4205584ff525dab83ec8
Sahil Ijaz <hssahil2913@gmail.com>
2025-04-25 15:58:23 +0500

    testing

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -34,3 +34,55 @@ exports.getOnePst = catchAsync(async (req, res, next) => {
     data: post,
   });
 });
+
+exports.updatePost = catchAsync(async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
+  post.caption = req.body.caption;
+  await post.save();
+  res.status(200).json({
+    message: "Requested post has been updated successfully.",
+    status: 200,
+    data: post,
+  });
+});
+
+exports.deletePost = catchAsync(async (req, res, next) => {
+  const post = await Post.findOneAndDelete({
+    _id: req.params.id,
+    creator: req.user._id,
+  });
+
+  if (!post) {
+    return next(new appError("Requested post was not found.", 404));
+  }
+
+  res.status(201).json({
+    message: "Requested post deleted successfully.",
+    status: 201,
+    data: null,
+  });
+});
+
+exports.deleteByAdmin = catchAsync(async (req, res, next) => {
+  const post = await Post.findByIdAndDelete(req.params.id);
+  if (!post) {
+    return next(new appError("Requested post was not deleted.", 404));
+  }
+
+  re.status(201).json({
+    message: "Requested post deleted successfully.",
+    status: 201,
+    data: null,
+  });
+});
+
+exports.ownAllPosts = catchAsync(async (req, res, next) => {
+  const posts = await Post.find({ creator: req.user._id });
+
+  res.status(200).json({
+    message: "All posts created by yu found successfully.",
+    status: 200,
+    length: posts.length,
+    data: posts,
+  });
+});
